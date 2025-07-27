9ce5cb4a55a3a2c1cb0e5ac4ddb0c9ca422bba6c
1533266228@qq.com <1533266228@qq.com>
2025-07-27 13:38:53 +0800

    完善

--- src/main/java/com/ectd/backend/service/testst.java
+++
@@ -1,8 +0,0 @@
-package com.ectd.backend.service;
-
-public class testst {
-    public static void main(String[] args) {
-        String sts = "yyyy-MM-ddTHH:mm:ss";
-        System.out.println(sts.substring(0,19).replace("T"," "));
-    }
-}
